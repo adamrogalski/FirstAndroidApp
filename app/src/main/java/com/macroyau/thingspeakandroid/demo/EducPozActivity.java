bbe9b1732587f10d0130c97deb7c471800f20438
adamrogalski <a.roglaski.94@gmail.com>
2018-10-17 00:06:17 +0200

    Retrofit

--- app/src/main/java/com/macroyau/thingspeakandroid/demo/EducPozActivity.java
+++ app/src/main/java/com/macroyau/thingspeakandroid/demo/EducPozActivity.java
@@ -4,31 +4,49 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
-import com.google.gson.JsonObject;
-import com.macroyau.thingspeakandroid.ThingSpeakService;
-import com.macroyau.thingspeakandroid.model.Channel;
-import com.macroyau.thingspeakandroid.model.ChannelFeed;
+import com.macroyau.thingspeakandroid.ThingSpeakServiceLastFeed;
 import com.macroyau.thingspeakandroid.model.Feed;
-import com.macroyau.thingspeakandroid.model.PublicChannels;
-import com.macroyau.thingspeakandroid.model.StatusUpdates;
 
 import java.util.List;
 import java.util.Map;
 
-import retrofit.Callback;
-import retrofit.client.Request;
+import retrofit2.Call;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.Response;
+import retrofit2.Retrofit;
 
 
 public class EducPozActivity extends AppCompatActivity {
+
+    private TextView temp;
+    private String test = "Test";
+
+    private Long channelId = 602058L;
+
+    private String baseUrl;
+    private ThingSpeakServiceLastFeed thingSpeakServiceLastFeed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_educ_poz);
 
+        temp = findViewById(R.id.temp_edu);
+        temp.setText(new String(" 21°C"));
 
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(baseUrl)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        ThingSpeakServiceLastFeed thingSpeakServiceLastFeed = retrofit.create(ThingSpeakServiceLastFeed.class);
+
+        Response<Feed> response = thingSpeakServiceLastFeed.getLastFeed(channelId).execute();
 
 
     }
 
-};
+
+
+
+}
